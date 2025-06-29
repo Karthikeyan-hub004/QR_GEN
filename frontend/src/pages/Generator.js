bfa8c0c248b7e22f1257ce69bbe2814e0ef09f75
karthikeyan <karthikeyana42004@gmail.com>
2025-06-29 10:04:56 +0530

    Update Generator.js

--- frontend/src/pages/Generator.js
+++ frontend/src/pages/Generator.js
@@ -83,4 +83,5 @@ const Generator = () => {
                   {...register('title', { required: 'Title is required' })}
                   type="text"
                   className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+  />
 
